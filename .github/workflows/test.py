201586ad904e1facf58149d75185596ff1e22fc1
parmod-github123 <162260947+parmod-github123@users.noreply.github.com>
2024-03-19 14:57:18 +0530

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,9 +1,11 @@
 import os
+import pysftp
 def main():
   print("Hello there")
   token = os.environ.get("TOKEN")
   print(token)
-
+  srv = pysftp.Connection(host="your_FTP_server", username="your_username",
+  password="your_password")
 
 if __name__ == '__main__':
   main()
