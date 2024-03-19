1ea9e1f169a51a56badc16a7d95dfd562ffeadec
parmod-github123 <162260947+parmod-github123@users.noreply.github.com>
2024-03-19 15:18:18 +0530

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -8,6 +8,9 @@ def main():
   else:
     print("token is good")
   
+# handling sftp connection for file deletion
+
+# https://www.datacourses.com/deleting-files-on-sftp-server-2469/
 
 if __name__ == '__main__':
   main()
